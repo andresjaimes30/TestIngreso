330902fb98c5931c75604b83d4956cf73673e6a3
andresjaimes30 <andres.jaimes30@hotmail.com>
2023-07-22 22:07:58 -0500

    Update TestIngresoApp.swift

--- TestIngreso/ViewModels/TestIngresoApp.swift
+++ TestIngreso/ViewModels/TestIngresoApp.swift
@@ -2,10 +2,23 @@ import SwiftUI
 
 @main
 struct TestIngresoApp: App {
+    // @State variable to control if the loading screen should be shown
+    @State private var showLoadingScreen = true
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-            
+            if showLoadingScreen {
+                LoadingView()
+                    .onAppear {
+                        // Add a delay here to simulate a loading time (optional)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            showLoadingScreen = false
+                        }
+                    }
+            } else {
+                // Your main ContentView or HomeView (the actual content of the app)
+                ContentView()
+            }
         }
     }
 }
